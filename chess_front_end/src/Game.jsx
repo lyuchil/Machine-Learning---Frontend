c7d571426a262eb2beedb34d16ee9f28748e9309
willhockey20 <willhockey122001@gmail.com>
2024-04-27 18:40:20 -0400

    Updated frontend with timer, setup for fetching model info, pawn promotion, confirmed castling and en passant work

--- chess_front_end/src/Game.jsx
+++ chess_front_end/src/Game.jsx
@@ -7,11 +7,75 @@ function Game({ players, room, orientation, cleanup }) {
   const chess = useMemo(() => new Chess(), []); // <- 1
   const [fen, setFen] = useState(chess.fen()); // <- 2
   const [over, setOver] = useState("");
+  const [playerTime, setPlayerTime] = useState({
+    w: 600
+  })
+  const [modelTime, setModelTime] = useState({
+    model:600
+  })
+  const [currentTurn, setCurrentTurn] = useState("w");
+  const [endByTime, setEndByTime] = useState({
+    w: false,
+    m: false
+  })
+
+  useEffect(()=> {
+    fetchModel();
+
+  }, []);
+
+  const fetchModel = async () =>{
+    try{
+      const gameBoard = chess.board();
+      const castlingRights = {
+        player: chess.getCastlingRights("w"),
+        model: chess.getCastlingRights("b")
+      }
+      const body = {
+        gameBoard,
+        castlingRights,
+        modelTime
+      }
+      const response = await fetch("/whatever/the/endpoint/is", {
+        method: 'POST',
+        body
+      })
+      const data = await response.json();
+      setModelTime(data.updatedModelTime)
+      onDrop(data.source, data.target, data.piece)
+    }
+    catch (error){
+      console.error("Failed to get move from model: ", error)
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() =>{
+      if(chess.isGameOver()){
+        clearInterval(timer);
+      }
+      else{
+        setPlayerTime(prevTime => ({
+          ...prevTime,
+          [currentTurn] : prevTime[currentTurn] - 1
+        }));
+      }
+    }, 1000);
+    return ()=>clearInterval(timer);
+  },[currentTurn, chess]);
 
   const makeMove = useCallback((move) => {
     try{
+      console.log(move)
         const rst = chess.move(move);
         setFen(chess.fen());
+        setCurrentTurn(chess.turn())
+        if(playerTime.w <=0){
+          setEndByTime({w:true, m:false})
+        }
+        else if(modelTime.model <=0){
+          setEndByTime({w:false,m:true})
+        }
 
         console.log("ov, cm", chess.isGameOver(), chess.isCheckmate());
         if(chess.isGameOver()){
@@ -27,6 +91,18 @@ function Game({ players, room, orientation, cleanup }) {
                 setOver("Game over");
             }
         }
+        else if(endByTime.w || endByTime.m){
+          if(endByTime.w){
+            setOver(
+              'Black won by time!'
+            );
+          }
+          else if(endByTime.m){
+            setOver(
+              'White won by time!'
+            )
+          }
+        }
 
         return rst;
     }
@@ -35,17 +111,22 @@ function Game({ players, room, orientation, cleanup }) {
     } 
   }, [chess]);
 
-  function onDrop(source, target) {
-    console.log(source, target)
+  function onDrop(source, target, piece) {
+    //console.log(source, target)
     const move_data = {
         from:source,
         to:target,
         color:chess.turn(),
-        promotion: "q"
+        promotion: piece.substring(1, 2).toLowerCase()
     };
-    console.log(chess.promotion)
+    console.log(move_data.color, move_data.promotion)
 
     const move = makeMove(move_data);
+
+    console.log(chess.board())
+    console.log(move)
+    console.log(chess.moves({verbose: true}))
+    console.log(chess.getCastlingRights("w"))
 
     if(move === null) return false;
     return true;
@@ -55,7 +136,13 @@ function Game({ players, room, orientation, cleanup }) {
   return (
     <>
       <div className="board">
-        <Chessboard position={fen} onPieceDrop={onDrop} />  {/**  <- 4 */}
+        <Chessboard position={fen} onPieceDrop={onDrop} /> 
+        <div className="timer">
+          White Time: {playerTime.w}s
+        </div>
+        <div className="timer">
+        Black Time: {modelTime.model}s
+        </div>
       </div>
       <CustomDialog // <- 5
         open={Boolean(over)}
